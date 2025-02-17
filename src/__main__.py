8454071b2db65654641828071157d58aa199fa1b
mtfs1 <math.santos.2002@gmail.com>
2025-02-17 08:35:04 -0300

    feat: dados mock de estoque e caixas

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
+from gera_dados_entrada import dados_de_caixas, dados_de_estoque
+
 
 def main():
     print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][START]")
+
+    estoque = dados_de_estoque()
+    caixas = dados_de_caixas()
+
     print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][END]")
 
 if __name__ == "__main__":
