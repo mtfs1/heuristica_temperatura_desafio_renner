d001fb171e53554eadf4be51ebaa915b65c85056
mtfs1 <math.santos.2002@gmail.com>
2025-02-16 23:59:51 -0300

    chore: initial commit

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def main():
+    print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][START]")
+    print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][END]")
+
+if __name__ == "__main__":
+    main()
+
