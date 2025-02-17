9f033dd6db4dbab17c95328d1fa68c7f5407fc8d
mtfs1 <math.santos.2002@gmail.com>
2025-02-17 19:21:06 -0300

    feat: gera documentos de reservas

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from alocacao_estoques import gera_documentos_reservas
 from alocacao_estoques import gera_estrutura_estoque
 from gera_dados_entrada import dados_de_caixas, dados_de_estoque
 
@@ -10,6 +11,7 @@ def main():
     estoque = dados_de_estoque()
     caixas = dados_de_caixas()
     estoque = gera_estrutura_estoque(estoque)
+    reservas = gera_documentos_reservas(caixas, 20, 6)
 
     print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][END]")
 
--- src/alocacao_estoques.py
+++ src/alocacao_estoques.py
@@ -28,3 +28,57 @@ def gera_estrutura_estoque(estoque: list) -> Estoque:
     return estrutura_estoque
 
 
+type Caixa = int
+type Item = int
+type Onda = int
+
+class Reserva(dict):
+    caixa: Caixa
+    item: Item
+    quantidade: QuantidadeItem
+    onda: Onda
+
+
+def gera_documentos_reservas(
+    caixas: list[tuple[Lote, int]], num_ondas: int, limite_onda: int
+) -> list[Reserva]:
+    print(f"[{datetime.now()}][GERA DOCUMENTOS RESERVAS]")
+
+    ondas = [
+        {"quantidade": 0, "classe": None} for _ in range(num_ondas)
+    ]
+    reservas = []
+    for c, (caixa, classe_onda) in enumerate(caixas):
+        #TODO: criar algoritmo imune a fagmentacao
+        onda_caixa = None
+        tamanho_caixa = sum(caixa)
+        for o, onda in enumerate(ondas):
+            if onda["quantidade"] + tamanho_caixa > limite_onda:
+                continue
+
+            if not onda["classe"] in [None, classe_onda]:
+                continue
+
+            onda["classe"] = classe_onda
+            onda["quantidade"] += tamanho_caixa
+            onda_caixa = o
+            break
+
+        if onda_caixa == None:
+            print("[ERROR][NAO PODE ACHAR ONDA PARA CAIXA]")
+            exit(1)
+
+        for item, quantidade in enumerate(caixa):
+            if quantidade == 0:
+                continue
+
+            reserva = {
+                "caixa": c,
+                "item": item,
+                "quantidade": quantidade,
+                "onda": onda_caixa,
+            }
+            reservas.append(reserva)
+
+    return reservas
+
