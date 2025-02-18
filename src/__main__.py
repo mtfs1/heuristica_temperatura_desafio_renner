80de5e131c3c9b313ce704554c2de5f9a0523943
mtfs1 <math.santos.2002@gmail.com>
2025-02-18 13:50:28 -0300

    feat: logica iteracoes heuristica

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,7 @@ from alocacao_estoques import alocacao_reservas
 from alocacao_estoques import calcula_funcao_objetivo
 from alocacao_estoques import gera_documentos_reservas
 from alocacao_estoques import gera_estrutura_estoque
+from alocacao_estoques import retira_reservas
 from gera_dados_entrada import dados_de_caixas, dados_de_estoque
 
 
@@ -13,10 +14,39 @@ def main():
     estoque = dados_de_estoque()
     caixas = dados_de_caixas()
     estoque = gera_estrutura_estoque(estoque)
-    reservas = gera_documentos_reservas(caixas, 20, 6)
+    reservas = gera_documentos_reservas(caixas, 1, 6000)
 
     estoque = alocacao_reservas(estoque, reservas, 0.5)
     objetivo = calcula_funcao_objetivo(estoque, 1, 1)
+
+    temperaturas = [
+        {
+            "probabilidade": 0.7,
+            "vezes": 100,
+        },
+        {
+            "probabilidade": 0.4,
+            "vezes": 200,
+        },
+        {
+            "probabilidade": 0.2,
+            "vezes": 400,
+        },
+    ]
+
+    print(f"[{datetime.now()}][SCORE][{objetivo}]")
+    for temperatura in temperaturas:
+        for _ in range(temperatura["vezes"]):
+            novo_estoque, retiradas = retira_reservas(
+                estoque, temperatura["probabilidade"]
+            )
+            novo_estoque = alocacao_reservas(novo_estoque, retiradas, 0.5)
+            novo_objetivo = calcula_funcao_objetivo(novo_estoque, 1, 1)
+
+            if novo_objetivo < objetivo:
+                objetivo = novo_objetivo
+                estoque = novo_estoque
+                print(f"[{datetime.now()}][SCORE][{objetivo}]")
 
     print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][END]")
 
