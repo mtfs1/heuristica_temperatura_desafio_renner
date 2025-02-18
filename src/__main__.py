f6419ea1e6ad0d1ac87bde97f08dee7f5d98efe9
mtfs1 <math.santos.2002@gmail.com>
2025-02-18 14:11:21 -0300

    refactor: le dados de entrada de arquivo csv

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,37 @@
+import os
 from datetime import datetime
+
+import pandas as pd
 
 from alocacao_estoques import alocacao_reservas
 from alocacao_estoques import calcula_funcao_objetivo
 from alocacao_estoques import gera_documentos_reservas
 from alocacao_estoques import gera_estrutura_estoque
 from alocacao_estoques import retira_reservas
-from gera_dados_entrada import dados_de_caixas, dados_de_estoque
+from gera_dados_entrada import transforma_dados_de_entrada
+
+
+dir_arquivos = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "arquivos",
+)
 
 
 def main():
     print(f"[{datetime.now()}][HEURISTICA TEMPERATURA][START]")
 
-    estoque = dados_de_estoque()
-    caixas = dados_de_caixas()
+    print(f"[{datetime.now()}][CARREGA CSV ESTOQUE]")
+    estoque = pd.read_csv(
+        os.path.join(dir_arquivos, "estoque_selected.csv")
+    )
+
+    print(f"[{datetime.now()}][CARREGA CSV CAIXAS]")
+    caixas = pd.read_csv(
+        os.path.join(dir_arquivos, "caixas_selected.csv")
+    )
+
+    estoque, caixas = transforma_dados_de_entrada(estoque, caixas)
     estoque = gera_estrutura_estoque(estoque)
     reservas = gera_documentos_reservas(caixas, 1, 6000)
 
--- src/gera_dados_entrada.py
+++ src/gera_dados_entrada.py
@@ -1,47 +1,74 @@
+import os
 from datetime import datetime
 
+import pandas as pd
 
-def dados_de_estoque() -> list:
+
+dir_arquivos = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "arquivos",
+)
+
+
+def enumera(df: pd.DataFrame, coluna: str) -> dict:
+    return {
+        val: i
+        for i, val in enumerate(df[coluna].sort_values().unique())
+    }
+
+
+def transforma_dados_de_entrada(
+    estoque: pd.DataFrame, caixas: pd.DataFrame
+) -> tuple[list, list]:
+    print(f"[{datetime.now()}][TRANSFORMA DADOS DE ENTRADA]")
+
+    todos_os_itens = pd.concat(
+        [estoque[["SKU"]], caixas[["SKU"]]],
+        ignore_index=True
+    )
+    itens = enumera(todos_os_itens, "SKU")
+
+    E = dados_de_estoque(estoque, itens)
+    C = dados_de_caixas(caixas, itens)
+
+    return E, C
+
+
+def dados_de_estoque(estoque: pd.DataFrame, itens: dict) -> list:
     print(f"[{datetime.now()}][DADOS DE ESTOQUE]")
 
+    andares = enumera(estoque, "ANDAR")
+    corredores = enumera(estoque, "CORREDOR")
+
     E = [
-        [
-            [10, 20, 30, 10, 20, 30],
-            [0, 10, 0, 10, 0, 10],
-            [30, 20, 10, 30, 20, 10],
-            [0, 30, 0, 10, 0, 20],
-        ],
-        [
-            [10, 20, 30, 10, 20, 30],
-            [0, 10, 0, 10, 0, 10],
-            [30, 20, 10, 30, 20, 10],
-            [0, 30, 0, 10, 0, 20],
-        ],
-        [
-            [10, 20, 30, 10, 20, 30],
-            [0, 10, 0, 10, 0, 10],
-            [30, 20, 10, 30, 20, 10],
-            [0, 30, 0, 10, 0, 20],
-        ],
+        [[0 for _ in itens] for _ in corredores]
+        for _ in andares
     ]
+
+    for _, linha in estoque.iterrows():
+        andar = andares[linha["ANDAR"]]
+        corredor = corredores[linha["CORREDOR"]]
+        item = itens[linha["SKU"]]
+        E[andar][corredor][item] = linha["PECAS"]
 
     return E
 
 
-def dados_de_caixas() -> list:
+def dados_de_caixas(caixas: pd.DataFrame, itens: dict) -> list:
     print(f"[{datetime.now()}][DADOS DE CAIXAS]")
 
-    C = [
-        ([2, 0, 0, 1, 0, 0], 1),
-        ([0, 1, 1, 1, 0, 0], 2),
-        ([0, 0, 3, 0, 0, 0], 3),
-        ([0, 0, 0, 2, 0, 1], 1),
-        ([0, 0, 0, 0, 3, 0], 2),
-        ([0, 0, 0, 0, 1, 2], 3),
-        ([1, 1, 1, 0, 0, 0], 1),
-        ([0, 1, 0, 0, 1, 1], 2),
-        ([0, 0, 0, 1, 0, 2], 3),
-    ]
+    ids = enumera(caixas, "CAIXA_ID")
+    classes = enumera(caixas, "CLASSE_ONDA")
+
+    C = [[[0 for _ in itens], 0] for _ in ids]
+
+    for _, linha in caixas.iterrows():
+        id = ids[linha["CAIXA_ID"]]
+        classe = classes[linha["CLASSE_ONDA"]]
+        item = itens[linha["SKU"]]
+        C[id][0][item] = linha["PECAS"]
+        C[id][1] = classe
 
     return C
 
